13a87beb1b6d091ec1f415a25a91520684ded353
garvitbansal <garvitbansal244@gmail.com>
2014-09-11 19:02:41 +0530

    Initial commit

---
+++ Java_Flight/src/flow/demandGraph/Edge.java
@@ -0,0 +1,33 @@
+/*
+Flights Scheduling using Max Flow
+*/
+
+package flow.demandGraph;
+
+public class Edge {
+
+	private int capacity;
+	private final String nextVertex;
+
+	public Edge(String nextVertex, int capacity) {
+		this.nextVertex = nextVertex;
+		this.capacity = capacity;
+	}
+
+	public String getNext() {
+		return nextVertex;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void decreaseCapacity(){
+		capacity--;
+	}
+
+	@Override
+	public String toString() {
+		return nextVertex +" C: "+capacity;
+	}
+}
---
+++ Java_Flight/src/flow/flightGraph/Flight.java
@@ -0,0 +1,73 @@
+/*
+Flights Scheduling using Max Flow
+*/
+
+package flow.flightGraph;
+
+
+/**
+ *	Represents a single flight.
+ *	Contains: origin, destination, departure and arrival time.
+ */
+public class Flight {
+
+	private String origin, dest; 
+	private Timestamp depTime;
+	private Timestamp arrTime;
+
+	/**
+	 * Flight constructor.
+	 * @param origin - Origin Airport
+	 * @param depHour - Departure Hours
+	 * @param depMin - Departure Minutes
+	 * @param dest - Destination Airport
+	 * @param arrHour - Arrival Hours
+	 * @param arrMin - Arrival Minutes
+	 */
+	public Flight(String origin, int depHour, int depMin, String dest, int arrHour, int arrMin) {
+		this.origin = origin;
+		this.dest = dest;
+		depTime = new Timestamp(depHour, depMin);
+		arrTime = new Timestamp(arrHour, arrMin);
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public String getDest() {
+		return dest;
+	}
+
+	public Timestamp getDepTime() {
+		return depTime;
+	}
+
+	public Timestamp getArrTime() {
+		return arrTime;
+	}
+
+
+	/**
+	 * Checks if the flight is reachable by flight fi.
+	 * A flight is reachable by another if there is a gap of 180 minutes in between.
+	 * @param fi - the flight to check reachability
+	 * @return true if there is a gap of 180 minutes
+	 */
+	public boolean isReachableBy(Flight fi) { 
+		return this.getDepTime().deferByMinutes(fi.getArrTime(),180);
+	}
+
+	/**
+	 * Checks if the flight is reachable by flight fi, given that it's origin is the same as
+	 * fi's destination and there is a gap of 60 minutes in between.
+	 * @param fi - the flight to check reachability
+	 * @return true if there is a gap of 60 minutes and airports match
+	 */
+	public boolean isSameAirportAndReachableBy(Flight fi) { 
+		return this.getOrigin().equals(fi.getDest()) 
+				&& this.getDepTime().deferByMinutes(fi.getArrTime(),60);
+	}
+
+
+}
